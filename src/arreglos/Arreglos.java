9476fc183d9897e8f03bf3231415c57b00cb0e97
alba <monserrat.rodriguez@uniceba.edu.mx>
2018-08-30 10:00:56 -0500

    arreglo

---
+++ src/arreglos/Arreglos.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author alba
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        String arreglo[] = new String[5];
+        arreglo[0] = "variable 1";
+        arreglo[1] = "variable 2";
+        arreglo[2] = "variable 3";
+        arreglo[3] = "variable 4";
+        arreglo[4] = "variable 5";
+       for (int contador = 0; contador < arreglo.length; contador++) {
+            System.out.println(arreglo[contador]);
+            System.out.println("**Arreglo 2");
+            String arreglo2[] = { "variable 1", "variable 2"};
+        
+        for (int conta = 0; conta < arreglo2.length; conta++) {
+            System.out.println(arreglo2[conta]);
+            
+            System.out.println("*arreglo 3");
+            int arreglo3[] = new int [6];
+        arreglo3[0] = 6;
+        arreglo3 [1] =5;
+        arreglo3[2] = 4;
+        arreglo3 [3] = 3;
+        arreglo3[4] = 2;
+        arreglo3 [5] = 1;
+      
+        for (int cont = 0; cont < arreglo3.length; cont++){
+            System.out.println(arreglo3[cont]);
+        
+        }
+        }
+       }
+    
+      
+        
+        
+        
+    
+    
+
+
